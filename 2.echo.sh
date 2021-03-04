37aa6fde3d5d90d6beddc6e85069dadd80713bb1
JManuelIzRa <jmanuel.izquierdo00@gmail.com>
2021-03-04 13:00:59 +0100

    echo basics

---
+++ 2.echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hola como estas?"
+echo -n "Yo bien, y tu?" # -n borra el salto de linea que pone por defecto echo
+echo -e "Este texto se ha separado \n usando el barra n" # -e permite que interprete el salto de linea usando \n
